734adf1bbf02b2d7e829776a7922e61416c2e884
Sebastian <sgarciaguerrero@safareyes.es>
2024-03-22 13:31:03 +0100

    Configurando Security

--- src/main/java/com/example/notiumb/security/config/SecurityConfig.java
+++ src/main/java/com/example/notiumb/security/config/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.example.notiumb.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    AuthenticationManager
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authManager) {
+    SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
         return http
                 .csrf().disable()
                 .authorizeRequests()
@@ -27,4 +33,13 @@ public class SecurityConfig {
 
     }
 
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin")
+                .password("admin")
+                .roles()
+                .build());
+        return manager;
+    }
 }
