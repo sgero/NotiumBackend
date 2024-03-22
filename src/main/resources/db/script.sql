d410eeb81f5959f4994fd706d46f991070b5596f
Sebastian <sgarciaguerrero@safareyes.es>
2024-03-22 23:43:51 +0100

    Script BBDD ejecutado con cambios y campos que faltaban

--- src/main/resources/db/script.sql
+++ src/main/resources/db/script.sql
@@ -1,15 +1,15 @@
-drop table if exists rpp;
-drop table if exists ocio_nocturno;
-drop table if exists cliente;
-drop table if exists restaurante;
-drop table if exists usuario;
 
+drop table if exists ocio_nocturno;
+drop table if exists restaurante;
+drop table if exists rpp;
+drop table if exists cliente;
+drop table if exists usuario;
 
 
 
 create table usuario
 (
-    id       serial not null,
+    id       serial       not null,
     username varchar(100) not null,
     email    varchar(100) not null,
     password varchar(100) not null,
@@ -19,69 +19,75 @@ create table usuario
 );
 
 
-create table cliente (
-    id serial not null,
-    nombre varchar(100) not null,
-    direccion varchar(100) not null,
-    telefono varchar(20) not null,
-    hora_apertura timestamp not null,
-    hora_cierre timestamp not null,
-    valoracion boolean,
-    disponible boolean,
-    imagen_marca varchar(1000) not null,
-    activo boolean,
-    id_usuario integer not null,
+create table cliente
+(
+    id               serial       not null,
+    nombre           varchar(100) not null,
+    apellidos        varchar(100) not null,
+    dni              varchar(9)   not null,
+    telefono         varchar(20)  not null,
+    fecha_nacimiento timestamp    not null,
+    direccion        varchar(100) not null,
+    activo           boolean,
+    id_usuario       integer      not null,
     primary key (id),
-    constraint id_cliente_usuario_fk foreign key (id_usuario) references usuario(id)
+    constraint id_cliente_usuario_fk foreign key (id_usuario) references usuario (id)
 );
 
-
-
-create table restaurante (
-    id serial not null,
-    nombre varchar(100) not null,
-    cif varchar(9) not null,
-    direccion varchar(200) not null,
-    telefono varchar(20) not null,
-    hora_apertura timestamp not null,
-    hora_cierre timestamp not null,
-    valoracion boolean,
-    disponible boolean,
-    imagen_marca varchar(1000) not null,
-    activo boolean,
+create table rpp
+(
+    id               serial       not null,
+    nombre           varchar(100) not null,
+    apellidos        varchar(100) not null,
+    dni              varchar(9)   not null,
+    telefono         varchar(20)  not null,
+    fecha_nacimiento timestamp    not null,
+    direccion        varchar(150) not null,
+    activo           boolean,
+    id_usuario       integer      not null,
     primary key (id),
-    id_usuario integer not null,
-    constraint id_restaurante_usuario_fk foreign key (id_usuario) references usuario(id)
+    constraint id_rpp_usuario_fk foreign key (id_usuario) references usuario (id)
+);
+
+create table restaurante
+(
+    id            serial        not null,
+    nombre        varchar(100)  not null,
+    cif           varchar(9)    not null,
+    direccion     varchar(200)  not null,
+    telefono      varchar(20)   not null,
+    hora_apertura timestamp     not null,
+    hora_cierre   timestamp     not null,
+    valoracion    boolean,
+    disponible    boolean,
+    imagen_marca  varchar(1000) not null,
+    activo        boolean,
+    id_usuario    integer       not null,
+    primary key (id),
+    constraint id_restaurante_usuario_fk foreign key (id_usuario) references usuario (id)
 );
 
 
 --Y el aforo???
-create table ocio_nocturno (
-    id serial not null,
-    nombre varchar(100) not null,
-    cif varchar(9) not null,
-    hora_apertura timestamp not null,
-    hora_cierre timestamp not null,
-    imagen_marca varchar(1000) not null,
-    activo boolean,
+create table ocio_nocturno
+(
+    id            serial        not null,
+    nombre        varchar(100)  not null,
+    cif           varchar(9)    not null,
+    direccion     varchar(200)  not null,
+    telefono      varchar(20)   not null,
+    hora_apertura timestamp     not null,
+    hora_cierre   timestamp     not null,
+    valoracion    boolean,
+    disponible    boolean,
+    imagen_marca  varchar(1000) not null,
+    activo        boolean,
+    id_usuario    integer       not null,
     primary key (id),
-    id_usuario integer not null,
-    constraint id_ocio_nocturno_usuario_fk foreign key (id_usuario) references usuario(id)
+    constraint id_ocio_nocturno_usuario_fk foreign key (id_usuario) references usuario (id)
 );
 
-create table rpp (
-    id serial not null,
-    nombre varchar(100) not null,
-    apellidos varchar(100) not null,
-    dni varchar(9) not null,
-    telefono varchar(20) not null,
-    fecha_nacimiento timestamp not null,
-    direccion varchar(150) not null,
-    activo boolean,
-    primary key (id),
-    id_usuario integer not null,
-    constraint id_rpp_usuario_fk foreign key (id_usuario) references usuario(id)
-);
+
 
 
 
