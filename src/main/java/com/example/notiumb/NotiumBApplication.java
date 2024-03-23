50b830d610fee1d883baa7de57560c2821b8040c
Sebastian <sgarciaguerrero@safareyes.es>
2024-03-23 20:32:32 +0100

    Correcciones para RUN de la APP (UserMapper + ahora da algun error del Springfox con el Swagger)

--- src/main/java/com/example/notiumb/NotiumBApplication.java
+++ src/main/java/com/example/notiumb/NotiumBApplication.java
@@ -2,7 +2,12 @@ package com.example.notiumb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -18,6 +23,14 @@ public class NotiumBApplication {
     @Configuration
     @EnableSwagger2
     public static class SwaggerConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("com.example.notiumb.controller"))
+                    .paths(PathSelectors.any())
+                    .build();
+        }
     }
 
 }
