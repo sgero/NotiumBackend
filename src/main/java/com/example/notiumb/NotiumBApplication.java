b5d325435d9c295eea6bfd3dfeba751c2a727a16
Sebastian <sgarciaguerrero@safareyes.es>
2024-03-24 13:43:39 +0100

    La ultima dependencia de springdoc-openapi-starter-common 2.4.0 es incompatible, por lo que la dejamos comentada.
    Solo falta encontrar lo que falta para que el enlace de swagger tire correctamente.
    Para ver el Swagger
    http://localhost:8080/swagger-ui/index.html

--- src/main/java/com/example/notiumb/NotiumBApplication.java
+++ src/main/java/com/example/notiumb/NotiumBApplication.java
@@ -1,16 +1,14 @@
 package com.example.notiumb;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
 
 @SpringBootApplication
 public class NotiumBApplication {
@@ -19,18 +17,18 @@ public class NotiumBApplication {
         SpringApplication.run(NotiumBApplication.class, args);
     }
 
+    @Bean
+    public GroupedOpenApi myApi() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
 
-//    @Configuration
-//    @EnableSwagger2
-//    public static class SwaggerConfig {
-//        @Bean
-//        public Docket api() {
-//            return new Docket(DocumentationType.SWAGGER_2)
-//                    .select()
-//                    .apis(RequestHandlerSelectors.basePackage("com.example.notiumb.controller"))
-//                    .paths(PathSelectors.any())
-//                    .build();
-//        }
-//    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API de Notium").description("API Notium: (Ocio Nocturno y Restaurantes)").version("1.0.0"));
+    }
 
 }
