e16b470ad12f9463d1efa65ba1e2c1a881cbeb28
Sebastian <sgarciaguerrero@safareyes.es>
2024-03-27 14:37:49 +0100

    User (usuario eliminado)

--- src/main/java/com/example/notiumb/repository/IUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.notiumb.repository;
-
-import com.example.notiumb.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUsuarioRepository extends JpaRepository<User, Integer> {
-}
