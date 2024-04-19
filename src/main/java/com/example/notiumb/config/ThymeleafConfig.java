541e13f89087436761bd7b8836ccc26f0c3cdab3
Sebas Garcia <sgarciaguerrero@safareyes.es>
2024-04-19 20:53:47 +0200

    arreglada la busqueda de ruta en resources > templates de las plantillas html para thymeleaf - 200 OK

--- src/main/java/com/example/notiumb/config/ThymeleafConfig.java
+++ src/main/java/com/example/notiumb/config/ThymeleafConfig.java
@@ -13,7 +13,10 @@ public class ThymeleafConfig {
     @Bean
     public TemplateEngine templateEngine() {
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
         // configure other properties of templateResolver if needed
 
         TemplateEngine templateEngine = new TemplateEngine();
