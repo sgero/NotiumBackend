86d01a514d5ff931ec06b8c40417c89ff82ab481
juanramonbaezaborrego <jbaezaborrego@safareyes.es>
2024-04-26 18:15:43 +0200

    Incluir Autowired en Repository

--- src/main/java/com/example/notiumb/service/ProductoService.java
+++ src/main/java/com/example/notiumb/service/ProductoService.java
@@ -1,7 +1,12 @@
 package com.example.notiumb.service;
 
+import com.example.notiumb.repository.IProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductoService {
+
+    @Autowired
+    private IProductoRepository iproductoRepository;
 }
--- src/main/java/com/example/notiumb/service/TurnoService.java
+++ src/main/java/com/example/notiumb/service/TurnoService.java
@@ -1,7 +1,13 @@
 package com.example.notiumb.service;
 
+import com.example.notiumb.repository.ITurnoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TurnoService {
+
+    @Autowired
+    private ITurnoRepository iTurnoRepository;
+
 }
