7b5d299ec643878455b3403bb6a52770fc216ebf
robeLocal <rbulloperez@safareyes.es>
2024-05-04 18:28:47 +0200

    RBP - 01/05/2024 - Implementación de metodo para verificar el aforo de un evento al comprar entradas

--- src/main/java/com/example/notiumb/repository/IEntradaOcioClienteRepository.java
+++ src/main/java/com/example/notiumb/repository/IEntradaOcioClienteRepository.java
@@ -2,8 +2,15 @@ package com.example.notiumb.repository;
 
 import com.example.notiumb.model.EntradaOcioCliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IEntradaOcioClienteRepository extends JpaRepository<EntradaOcioCliente, Integer> {
+    @Query(value = "select count(e.id) from notium.entrada_ocio_cliente e " +
+            "join notium.entrada_ocio eo on eo.id = e.id_entrada_ocio " +
+            "where eo.id = :id and eo.activo = true;", nativeQuery = true)
+    Integer cantidadEntradasVendidas(Integer id);
+
+
 }
