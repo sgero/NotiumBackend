68dba5c300daa71338e115809c158658003ee56c
Sebas Garcia <sgarciaguerrero@safareyes.es>
2024-05-18 12:28:22 +0200

    SGG - 16/05/2024 - Cambios en EmailController Service y DTO ON

--- src/main/java/com/example/notiumb/service/implementation/EmailServiceImpl.java
+++ src/main/java/com/example/notiumb/service/implementation/EmailServiceImpl.java
@@ -1,15 +1,20 @@
 package com.example.notiumb.service.implementation;
 
 
+import com.example.notiumb.model.User;
 import com.example.notiumb.service.IEmailService;
 import com.example.notiumb.dto.EmailDTO;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import java.util.UUID;
 
 @Service
 public class EmailServiceImpl implements IEmailService {
@@ -46,5 +51,22 @@ public class EmailServiceImpl implements IEmailService {
         } catch (Exception e) {
             throw new RuntimeException("Error sending email" + e.getMessage(), e);
         }
+    }
+
+//    @Autowired
+//    private JavaMailSender javaMailSender;
+
+    public void enviarEmailVerificacion(User usuario) {
+        // Lógica para enviar email de verificación con un enlace único
+        SimpleMailMessage mensaje = new SimpleMailMessage();
+        mensaje.setTo(usuario.getEmail());
+        mensaje.setSubject("Verifica tu cuenta");
+        mensaje.setText("Por favor haz clic en el siguiente enlace para verificar tu cuenta: http://localhost:8080/verificar?token=" + generarToken(usuario));
+        javaMailSender.send(mensaje);
+    }
+
+    private String generarToken(User usuario) {
+        // Lógica para generar un token único
+        return UUID.randomUUID().toString();
     }
 }
