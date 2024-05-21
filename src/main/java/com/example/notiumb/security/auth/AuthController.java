5d5260f05dc50443436c525e2421708a9ce089de
Sebas Garcia <sgarciaguerrero@safareyes.es>
2024-05-21 22:21:57 +0200

    SGG - 21/05/2024 - AuthController y Service para incorporación de token al front

--- src/main/java/com/example/notiumb/security/auth/AuthController.java
+++ src/main/java/com/example/notiumb/security/auth/AuthController.java
@@ -4,8 +4,11 @@ package com.example.notiumb.security.auth;
 import com.example.notiumb.dto.LoginDTO;
 import com.example.notiumb.dto.UserDTO;
 import com.example.notiumb.security.service.AuthenticationService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,21 @@ public class AuthController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @Getter
+    public static class LoginRequest {
+        private String username;
+        private String password;
+
+        // Getters y Setters
+    }
+
+
+    public static class LoginResponse {
+        public LoginResponse(String role) {
+        }
+        // Getters y Setters
+    }
+
 
     @PostMapping("/registrocliente")
     public AuthenticationResponseDTO register(@RequestBody UserDTO userDTO){
@@ -29,11 +47,28 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public AuthenticationResponseDTO register(@RequestBody LoginDTO loginDTO){
-        if(authenticationService.verifyPassword(loginDTO)){
+    public AuthenticationResponseDTO login(@RequestBody LoginDTO loginDTO) {
+        if (authenticationService.verifyPassword(loginDTO)) {
             return authenticationService.login(loginDTO);
-        }else{
+        } else {
             return AuthenticationResponseDTO.builder().message("Invalid credentials").build();
         }
     }
+
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+//        // Lógica para autenticar al usuario y obtener su rol
+//        String role = authenticationService.authenticateUser(loginRequest.getUsername(), loginRequest.getPassword());
+//
+//        if (role != null) {
+//            return ResponseEntity.ok(new LoginResponse(role));
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//        }
+//    }
+
+
+
+
 }
