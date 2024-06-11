276cc9e673dd34457c4d0d89d1f1f9343ca3df79
Eduardo <eruizgarcia@safareyes.es>
2024-06-11 16:31:28 +0200

    Areglos varios grupo usuario 11/06/2024

--- src/main/java/com/example/notiumb/repository/IRppRepository.java
+++ src/main/java/com/example/notiumb/repository/IRppRepository.java
@@ -1,7 +1,9 @@
 package com.example.notiumb.repository;
 
+import com.example.notiumb.model.Restaurante;
 import com.example.notiumb.model.Rpp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +16,9 @@ public interface IRppRepository extends JpaRepository <Rpp, Integer> {
 
     Optional<Rpp> findByIdAndActivoIsTrue(Integer id);
 
-   // Rpp findByIdUserAndActivoTrue(Integer id);
+    @Query(nativeQuery = true, value = "SELECT r.* FROM notium.rpp r\n" +
+            "WHERE r.id_usuario = :id and activo = true;")
+    Rpp findByIdUser(Integer id);
 
     List<Rpp> findAllByOcioNocturnoIdAndActivoIsTrue (Integer idOcio);
 
